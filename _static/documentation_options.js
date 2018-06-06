407e95c8e7437f66de643263f2fe1f1fd66eecda
Anthony Scopatz <scopatz@gmail.com>
2018-06-06 13:15:37 -0400

    GitHub pages update for 0.6.6

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: '',
-    VERSION: '0.6.4.dev20',
+    VERSION: '0.6.5.dev4',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
