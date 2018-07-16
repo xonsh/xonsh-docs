3b157a48b0ff15508940c88245ac07d921e18f85
Travis CI User <travis@example.org>
2018-07-16 16:23:34 +0000

    Update docs after building Travis build 7181 of xonsh/xonsh
    The docs were built from the branch 'master' against the commit
    bacf36c49c97b52f82ccf08356612befa42b5980.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/xonsh/xonsh/jobs/404516985.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.3/bin/doctr deploy --deploy-repo xonsh/xonsh-docs dev

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.6.10.dev47',
+    VERSION: '0.6.10.dev63',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     FILE_SUFFIX: '.html',
