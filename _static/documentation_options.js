4ac0ef7c7be18f216f5c5bc27b8236c4b4b9cea3
Anthony Scopatz <scopatz@gmail.com>
2020-03-19 15:41:50 -0500

    GitHub pages update for 0.9.15

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,9 +1,8 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.14',
+    VERSION: '0.9.12.dev70',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
-    BUILDER: 'html',
     FILE_SUFFIX: '.html',
     HAS_SOURCE: true,
     SOURCELINK_SUFFIX: '.txt',
