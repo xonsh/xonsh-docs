fe690082c39f003cf6eb90f72f1d3eaaa3d9a985
Doctr (Travis CI) <drdoctr@users.noreply.github.com>
2020-09-23 17:39:06 +0000

    Update docs after building Travis build 9935 of xonsh/xonsh
    The docs were built from the branch 'master' against the commit
    17e2cb0d039c9a89b7bde9a7e4fc9536877a38c1.
    
    The Travis build that generated this commit is at
    https://travis-ci.org/xonsh/xonsh/jobs/729703755.
    
    The doctr command that was run is
    
        /home/travis/virtualenv/python3.6.7/bin/doctr deploy --deploy-repo xonsh/xonsh-docs dev

--- dev/_static/documentation_options.js
+++ dev/_static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '0.9.22.dev91.dev91',
+    VERSION: '0.9.22.dev98.dev98',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
